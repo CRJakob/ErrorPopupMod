cb253b4f5e287f893c788f31ed52b1b5b92ca393
GameBuilder202 <unkown.sr71@gmail.com>
2025-04-27 20:13:52 +0530

    Add dev files

---
+++ pandoc-filter.lua
@@ -0,0 +1,9 @@
+Div = function(block)
+    block.attributes['style'] = "color: #ffffff; padding: 0.5em; font-size: 22px"
+    return block
+end
+
+Header = function(heading)
+    heading.attributes['style'] = "color: #ffffff; text-align: center"
+    return heading
+end
